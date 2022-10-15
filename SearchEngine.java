3eb60e2dbf1d1d43a5b65e916c0f2ea7b3dd4206
stareen1 <stareen@ucsd.edu>
2022-10-14 18:03:56 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 class Handler implements URLHandler {
   ArrayList<String> stringList = new ArrayList<>();
@@ -16,7 +18,7 @@ class Handler implements URLHandler {
             if (url.getPath().contains("/add")) {
                 String[] parameters = url.getQuery().split("=");
                 if (parameters[0].equals("s")) {
-                    stringList.get(stringList.size() - 1) = parameters[1];
+                    stringList.add(parameters[1]);
                     return "Added " + parameters[1] + " to string list!";
                 }
             }
@@ -25,7 +27,7 @@ class Handler implements URLHandler {
                 if (parameters[0].equals("s")) {
                     String matchedStrings = "";
                     for (int i = 0; i < stringList.size(); i++) {
-                      if stringList.get(i).contains(parameters[1]) {
+                      if (stringList.get(i).contains(parameters[1])) {
                         matchedStrings = matchedStrings + " " + stringList.get(i) + " ";
                       }
                     }
